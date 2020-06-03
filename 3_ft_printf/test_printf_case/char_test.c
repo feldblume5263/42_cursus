9c26636eb1e05f3fd50bf9a51ce1f2cb1f214676
feldblume5263 <53016167+feldblume5263@users.noreply.github.com>
2020-06-03 13:56:08 +0900

    add char_test

--- 3_ft_printf/test_printf_case/char_test.c
+++ 3_ft_printf/test_printf_case/char_test.c
@@ -6,7 +6,7 @@
 /*   By: junhpark <junhpark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/02 17:52:07 by junhpark          #+#    #+#             */
-/*   Updated: 2020/06/02 18:00:14 by junhpark         ###   ########.fr       */
+/*   Updated: 2020/06/03 13:55:43 by junhpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,221 +15,61 @@
 int		main(void)
 {
 	printf("***************************************\n");
-	printf("*********** printf_char_test ***********\n");
+	printf("*********** printf_char_test **********\n");
 	printf("***************************************\n");
 	printf("made by junhpark @ 42Seoul");
 	printf("\n");
 
 	printf("\n***************************************\n");
-	printf("\n0. printf(\"%%c\", ...);\n");
 
 	printf("\nprintf(\"%%c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%c|\n", 'a'));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%c|\n", 'a') - 14);
 
-	printf("\n***************************************\n");
-	printf("\n1. printf(\"%%(num)c\", ...);\n");
+	printf("\nprintf(\"%%0c\", 'a')\n");
+	/*printf("\t\treturn : %d\n", printf("\t\tresult : |%0c|\n", 'a') - 14);*/ printf("warnging\n");
 
-	printf("\nprintf(\"%%5c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%5c|\n", 'a'));
-
-	printf("\nprintf(\"%%7c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%7c|\n", 'a'));
+	printf("\nprintf(\"%%1c\", 'a')\n");
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%1c|\n", 'a') - 14);
 
 	printf("\nprintf(\"%%3c\", 'a')\n");
-	printf("\t\treturn : %c", printf("\t\tresult : |%3c|\n", 'a'));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%3c|\n", 'a') - 14);
 
-	printf("\nprintf(\"%%0c\", 'a')\n");
-	/*printf("\t\tresult : |%0c|\n", 'a');*/ printf("warning\n");
+	printf("\nprintf(\"%%5c\", 'a')\n");
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%5c|\n", 'a') - 14);
 
-	printf("\n***************************************\n");
-	printf("\n2. printf(\"%%-(num)c\", ...);\n");
+	printf("\nprintf(\"%%c\", 0)\n");
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%c|\n", 0) - 14);
+
+	printf("\nprintf(\"%%3c\", 0)\n");
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%3c|\n", 0) - 14);
+
+	printf("\nprintf(\"%%c\", '\\0')\n");
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%c|\n", '\0') - 14);
+
+	printf("\nprintf(\"%%3c\", '\\0')\n");
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%3c|\n", '\0') - 14);
 
 	printf("\nprintf(\"%%-c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%-c|\n", 'a'));
-
-	printf("\nprintf(\"%%-5c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%-5c|\n", 'a'));
-
-	printf("\nprintf(\"%%-7c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%-7c|\n", 'a'));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%-c|\n", 'a') - 14);
 
 	printf("\nprintf(\"%%-3c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%-3c|\n", 'a'));
-
-	printf("\nprintf(\"%%-0c\", 'a')\n");
-	/*printf("\t\tresult : |%-0c|\n", 'a');*/ printf("warning\n");
-
-	printf("\nprintf(\"%%---7c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%--7c|\n", 'a'));
-
-	printf("\nprintf(\"%%--3c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%--3c|\n", 'a'));
-
-	printf("\n***************************************\n");
-	printf("\n3. printf(\"%%.(num)c\", ...);\n");
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%-3c|\n", 'a') - 14);
 
 	printf("\nprintf(\"%%.c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%.c|\n", 'a'));
-
-	printf("\n**printf(\"%%.7c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%.7c|\n", 'a'));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%.c|\n", 'a') - 14);
 
 	printf("\nprintf(\"%%.3c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%.3c|\n", 'a'));
-
-	printf("\nprintf(\"%%.0c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%.0c|\n", 'a'));
-
-	printf("\n**printf(\"%%-.7c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%-.7c|\n", 'a'));
-
-	printf("\nprintf(\"%%-7.c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%-7.c|\n", 'a'));
-
-	printf("\nprintf(\"%%-.3c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%-.3c|\n", 'a'));
-
-	printf("\nprintf(\"%%-3.c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%-3.c|\n", 'a'));
-
-	printf("\nprintf(\"%%7.7c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%7.7c|\n", 'a'));
-
-	printf("\nprintf(\"%%7.3c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%7.3c|\n", 'a'));
-
-	printf("\nprintf(\"%%3.7c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%3.7c|\n", 'a'));
-
-	printf("\n**printf(\"%%9.7c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%9.7c|\n", 'a'));
-
-	printf("\n**printf(\"%%7.9c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%7.9c|\n", 'a'));
-
-	printf("\nprintf(\"%%-7.3c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%-7.3c|\n", 'a'));
-
-	printf("\nprintf(\"%%-9.7c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%-9.7c|\n", 'a'));
-
-	printf("\nprintf(\"%%-9.3c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%-9.3c|\n", 'a'));
-
-	printf("\nprintf(\"%%3.9c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%3.9c|\n", 'a'));
-
-	printf("\nprintf(\"%%-3.9c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%-3.9c|\n", 'a'));
-
-	printf("\n***************************************\n");
-	printf("\n4. printf(\"%%0c\", ...);\n");
-
-	printf("\nprintf(\"%%0c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%0c|\n", 'a'));
-
-	printf("\nprintf(\"%%07c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%07c|\n", 'a'));
-
-	printf("\nprintf(\"%%05c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%05c|\n", 'a'));
-
-	printf("\nprintf(\"%%03c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%03c|\n", 'a'));
-
-	printf("\nprintf(\"%%0.9c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%0.9c|\n", 'a'));
-
-	printf("\nprintf(\"%%0.5c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%0.5c|\n", 'a'));
-
-	printf("\nprintf(\"%%3.0c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%c|\n", 'a'));
-
-	printf("\nprintf(\"%%5.0c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%5.0c|\n", 'a'));
-
-	printf("\nprintf(\"%%7.0c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%7.0c|\n", 'a'));
-
-	printf("\nprintf(\"%%7.3c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%7.3c|\n", 'a'));
-
-	printf("\nprintf(\"%%7.5c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%7.5c|\n", 'a'));
-
-	printf("\nprintf(\"%%7.7c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%7.7c|\n", 'a'));
-
-	printf("\nprintf(\"%%5.3c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%5.3c|\n", 'a'));
-
-	printf("\nprintf(\"%%5.5c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%5.5c|\n", 'a'));
-
-	printf("\nprintf(\"%%5.7c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%5.7c|\n", 'a'));
-
-	printf("\nprintf(\"%%3.3c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%3.3c|\n", 'a'));
+	/*printf("\t\treturn : %d\n", printf("\t\tresult : |%.3c|\n", 'a') - 14);*/ printf("warning\n");
 
 	printf("\nprintf(\"%%3.5c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%3.5c|\n", 'a'));
+	/*printf("\t\treturn : %d\n", printf("\t\tresult : |%3.5c|\n", 'a') - 14);*/ printf("warning\n");
 
-	printf("\nprintf(\"%%3.7c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%3.7c|\n", 'a'));
+	printf("\nprintf(\"%%5.c\", 'a')\n");
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%5.c|\n", 'a') - 14);
 
-	printf("\nprintf(\"%%-0c\", 'a')\n");
-	/*printf("|%-0c|\n", 'a');*/ printf("warning\n");
+	printf("\nprintf(\"%%0c\", 'a')\n");
+	/*printf("\t\treturn : %d\n", printf("\t\tresult : |%0c|\n", 'a') - 14);*/ printf("warning\n");
 
-	printf("\nprintf(\"%%-07c\", 'a')\n");
-	/*printf("|%-07c|\n", 'a');*/ printf("warning\n");
-
-	printf("\nprintf(\"%%-05c\", 'a')\n");
-	/*printf("|%-05c|\n", 'a');*/ printf("warning\n");
-
-	printf("\nprintf(\"%%-03c\", 'a')\n");
-	/*printf("|%-03c|\n", 'a');*/ printf("warning\n");
-
-	printf("\nprintf(\"%%-0.9c\", 'a')\n");
-	/*printf("|%-0.9c|\n", 'a'));*/ printf("warning\n");
-
-	printf("\nprintf(\"%%-0.5c\", 'a')\n");
-	/*printf("|%-0.5c|\n", 'a');*/ printf("warning\n");
-
-	printf("\nprintf(\"%%-3.0c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%-3.0c|\n", 'a'));
-
-	printf("\nprintf(\"%%-5.0c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%-5.0c|\n", 'a'));
-
-	printf("\nprintf(\"%%-7.0c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%-7.0c|\n", 'a'));
-
-	printf("\nprintf(\"%%-7.3c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%-7.3c|\n", 'a'));
-
-	printf("\nprintf(\"%%-7.5c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%-7.5c|\n", 'a'));
-
-	printf("\nprintf(\"%%-7.7c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%-7.7c|\n", 'a'));
-
-	printf("\nprintf(\"%%-5.3c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%-5.3c|\n", 'a'));
-
-	printf("\nprintf(\"%%-5.5c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%-5.5c|\n", 'a'));
-
-	printf("\nprintf(\"%%-5.7c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%-5.7c|\n", 'a'));
-
-	printf("\nprintf(\"%%-3.3c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%-3.3c|\n", 'a'));
-
-	printf("\nprintf(\"%%-3.5c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%-3.5c|\n", 'a'));
-
-	printf("\nprintf(\"%%-3.7c\", 'a')\n");
-	printf("\t\treturn : %c\n", printf("\t\tresult : |%-3.7c|\n", 'a'));
+	printf("\nprintf(\"%%03c\", 'a')\n");
+	/*printf("\t\treturn : %d\n", printf("\t\tresult : |%03c|\n", 'a') - 14);*/ printf("warning\n");
 }
--- 3_ft_printf/test_printf_case/int_test.c
+++ 3_ft_printf/test_printf_case/int_test.c
@@ -6,7 +6,7 @@
 /*   By: junhpark <junhpark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/01 13:09:21 by junhpark          #+#    #+#             */
-/*   Updated: 2020/06/02 19:38:26 by junhpark         ###   ########.fr       */
+/*   Updated: 2020/06/03 13:11:08 by junhpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,19 +24,19 @@ int		main(void)
 	printf("\n0. printf(\"%%d\", ...);\n");
 
 	printf("\nprintf(\"%%d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%d|\n", 12345) - 14);
 
 	printf("\n***************************************\n");
 	printf("\n1. printf(\"%%(num)d\", ...);\n");
 
 	printf("\nprintf(\"%%5d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%5d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%5d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%7d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%7d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%7d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%3d\", 12345)\n");
-	printf("\t\treturn : %d", printf("\t\tresult : |%3d|\n", 12345));
+	printf("\t\treturn : %d", printf("\t\tresult : |%3d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%0d\", 12345)\n");
 	/*printf("\t\tresult : |%0d|\n", 12345);*/ printf("warning\n");
@@ -45,139 +45,139 @@ int		main(void)
 	printf("\n2. printf(\"%%-(num)d\", ...);\n");
 
 	printf("\nprintf(\"%%-d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%-d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%-d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%-5d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%-5d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%-5d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%-7d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%-7d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%-7d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%-3d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%-3d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%-3d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%-0d\", 12345)\n");
 	/*printf("\t\tresult : |%-0d|\n", 12345);*/ printf("warning\n");
 
 	printf("\nprintf(\"%%---7d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%--7d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%--7d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%--3d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%--3d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%--3d|\n", 12345) - 14);
 
 	printf("\n***************************************\n");
 	printf("\n3. printf(\"%%.(num)d\", ...);\n");
 
 	printf("\nprintf(\"%%.d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%.d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%.d|\n", 12345) - 14);
 
 	printf("\n**printf(\"%%.7d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%.7d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%.7d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%.3d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%.3d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%.3d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%.0d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%.0d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%.0d|\n", 12345) - 14);
 
 	printf("\n**printf(\"%%-.7d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%-.7d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%-.7d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%-7.d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%-7.d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%-7.d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%-.3d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%-.3d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%-.3d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%-3.d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%-3.d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%-3.d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%7.7d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%7.7d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%7.7d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%7.3d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%7.3d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%7.3d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%3.7d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%3.7d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%3.7d|\n", 12345) - 14);
 
 	printf("\n**printf(\"%%9.7d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%9.7d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%9.7d|\n", 12345) - 14);
 
 	printf("\n**printf(\"%%7.9d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%7.9d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%7.9d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%-7.3d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%-7.3d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%-7.3d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%-9.7d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%-9.7d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%-9.7d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%-9.3d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%-9.3d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%-9.3d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%3.9d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%3.9d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%3.9d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%-3.9d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%-3.9d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%-3.9d|\n", 12345) - 14);
 
 	printf("\n***************************************\n");
 	printf("\n4. printf(\"%%0d\", ...);\n");
 
 	printf("\nprintf(\"%%0d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%0d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%0d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%07d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%07d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%07d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%05d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%05d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%05d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%03d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%03d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%03d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%0.9d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%0.9d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%0.9d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%0.5d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%0.5d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%0.5d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%3.0d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%5.0d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%5.0d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%5.0d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%7.0d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%7.0d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%7.0d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%7.3d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%7.3d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%7.3d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%7.5d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%7.5d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%7.5d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%7.7d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%7.7d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%7.7d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%5.3d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%5.3d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%5.3d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%5.5d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%5.5d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%5.5d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%5.7d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%5.7d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%5.7d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%3.3d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%3.3d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%3.3d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%3.5d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%3.5d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%3.5d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%3.7d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%3.7d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%3.7d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%-0d\", 12345)\n");
 	/*printf("|%-0d|\n", 12345);*/ printf("warning\n");
@@ -192,44 +192,44 @@ int		main(void)
 	/*printf("|%-03d|\n", 12345);*/ printf("warning\n");
 
 	printf("\nprintf(\"%%-0.9d\", 12345)\n");
-	/*printf("|%-0.9d|\n", 12345));*/ printf("warning\n");
+	/*printf("|%-0.9d|\n", 12345) - 14);*/ printf("warning\n");
 
 	printf("\nprintf(\"%%-0.5d\", 12345)\n");
 	/*printf("|%-0.5d|\n", 12345);*/ printf("warning\n");
 
 	printf("\nprintf(\"%%-3.0d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%-3.0d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%-3.0d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%-5.0d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%-5.0d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%-5.0d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%-7.0d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%-7.0d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%-7.0d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%-7.3d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%-7.3d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%-7.3d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%-7.5d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%-7.5d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%-7.5d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%-7.7d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%-7.7d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%-7.7d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%-5.3d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%-5.3d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%-5.3d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%-5.5d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%-5.5d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%-5.5d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%-5.7d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%-5.7d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%-5.7d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%-3.3d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%-3.3d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%-3.3d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%-3.5d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%-3.5d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%-3.5d|\n", 12345) - 14);
 
 	printf("\nprintf(\"%%-3.7d\", 12345)\n");
-	printf("\t\treturn : %d\n", printf("\t\tresult : |%-3.7d|\n", 12345));
+	printf("\t\treturn : %d\n", printf("\t\tresult : |%-3.7d|\n", 12345) - 14);
 }
