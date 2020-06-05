f0329cfee984bebd5f844cb0412d025c57f56274
feldblume5263 <53016167+feldblume5263@users.noreply.github.com>
2020-06-06 00:29:24 +0900

    update code with new version

--- 3_ft_printf/ft_printf.c
+++ 3_ft_printf/ft_printf.c
@@ -6,35 +6,95 @@
 /*   By: junhpark <junhpark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/31 14:57:54 by junhpark          #+#    #+#             */
-/*   Updated: 2020/06/04 16:40:47 by junhpark         ###   ########.fr       */
+/*   Updated: 2020/06/06 00:27:59 by junhpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include "libft.h"
 
-char		*ft_put_string(char **format)
+#include <stdio.h>
+
+int			inspect_chunk(char *chr, char *chunk)
 {
-	char			**str;
 	size_t			idx;
 
 	idx = 0;
-	while (*format[idx])
+	while (chunk[idx])
 	{
-		if (*format[idx] == '%')
-		{
-			idx++;
-			convert_to_string();
-		}
+		if (chunk[idx] == *chr)
+			return (TRUE);
 		idx++;
 	}
+	return (FALSE);
+}
+
+int			get_data_len(char *format)
+{
+	int				data_len;
+
+	data_len = 0;
+	while (inspect_chunk(&(format[data_len]), FLAG) == TRUE)
+		data_len++;
+	data_len++;
+	return (data_len);
+}
+
+char		*prepare_data(char *format, int data_len)
+{
+	char			*data;
+	int				idx;
+
+	data = (char *)malloc(sizeof(char) * (data_len + 1));
+	idx = 0;
+	while (idx < data_len)
+	{
+		data[idx] = format[idx];
+		idx++;
+	}
+	data[idx] = NLL;
+	return (data);
+}
+
+int			allocate_and_write_by_conv(char *format, va_list ap)
+{
+	char			*data;
+	int				ret_count;
+	int				data_len;
+	int				idx;
+
+	idx = 0;
+	while (format[idx])
+	{
+		while (format[idx] != '%' && format[idx])
+		{
+			write(1, &(format[idx]), 1);
+			idx++;
+		}
+		idx++;
+		data_len = get_data_len(&(format[idx]));
+		printf("\nlen = %d\n", data_len);
+		data = prepare_data(&(format[idx]), data_len);
+	}
+	return (0);
 }
 
 int			ft_printf(const char *format, ...)
 {
 	char			*res_string;
+	int				str_len;
 	va_list			ap;
 
 	va_start(ap, format);
-	res_string = ft_put_string(&(*format));
+	str_len = allocate_and_write_by_conv((char *)format, ap);
 	va_end(ap);
+	return (str_len);
+}
+
+int			main(void)
+{
+	int		return_value;
+
+	return_value = ft_printf("\nBalea %05.5d Handcreme\n", 2014);
+	printf("return value = %d\n", return_value);
 }
--- 3_ft_printf/ft_printf.h
+++ 3_ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: junhpark <junhpark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/03 16:34:16 by junhpark          #+#    #+#             */
-/*   Updated: 2020/06/04 15:16:32 by junhpark         ###   ########.fr       */
+/*   Updated: 2020/06/05 21:18:56 by junhpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@
 # define FALSE 0
 # define ERROR -1
 
-# define NULL '\0'
+# define NLL '\0'
 
 typedef struct	s_flag
 {
@@ -35,5 +35,11 @@ typedef struct	s_flag
 	int		padding_left;
 	int		padding_right;
 }				t_flag;
+
+int			ft_printf(const char *format, ...);
+int			allocate_write_by_conv(char *format, va_list ap);
+int			inspect_chunk(char *chr, char *chunk);
+char		*prepare_data(char *format, int data_len);
+int			get_data_len(char *format);
 
 # endif
