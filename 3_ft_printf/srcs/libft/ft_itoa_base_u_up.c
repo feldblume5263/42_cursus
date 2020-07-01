beed160ef617d3365eb112201c79a0b87d57bbb5
feldblume5263 <53016167+feldblume5263@users.noreply.github.com>
2020-07-01 17:20:57 +0900

    update codes

--- 3_ft_printf/srcs/libft/ft_itoa_base_u_up.c
+++ 3_ft_printf/srcs/libft/ft_itoa_base_u_up.c
@@ -1,24 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_itoa_base_ll.c                                  :+:      :+:    :+:   */
+/*   ft_itoa_base_u_up.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: junhpark <junhpark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/06/30 16:35:33 by junhpark          #+#    #+#             */
-/*   Updated: 2020/07/01 17:17:17 by junhpark         ###   ########.fr       */
+/*   Created: 2020/07/01 17:19:15 by junhpark          #+#    #+#             */
+/*   Updated: 2020/07/01 17:20:18 by junhpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	get_len_base_ll(long long num)
+static size_t	get_len_base_u(unsigned int num)
 {
 	size_t				len;
 	unsigned long long	cp_num;
 
 	len = 0;
-	cp_num = num;
+	cp_num = (unsigned long long)num;
 	if (cp_num < 0)
 	{
 		len++;
@@ -32,30 +32,25 @@ static size_t	get_len_base_ll(long long num)
 	return (++len);
 }
 
-char			*ft_itoa_base_ll(long long num)
+char			*ft_itoa_base_u(unsigned int num)
 {
 	size_t				len;
 	size_t				idx;
 	unsigned long long	cp_num;
 	char				*result;
 
-	len = get_len_base_ll(num);
+	len = get_len_base_u(num);
+	cp_num = (unsigned long long)num;
 	if (!(result = (char *)malloc(sizeof(char) * (len + 1))))
 		return (0);
 	result[len] = 0;
 	idx = 0;
-	cp_num = num < 0 ? (unsigned long long)num * -1 : (unsigned long long)num;
 	while (idx < len)
 	{
-		if (idx == len - 1 && num < 0)
-		{
-			result[0] = '-';
-			break ;
-		}
 		if (cp_num % 16 >= 0 && cp_num % 16<= 9)
 			result[len - idx - 1] = cp_num % 16 + '0';
 		else if (cp_num % 16 >= 10 && cp_num % 16 <= 15)
-			result[len - idx - 1] = cp_num % 16 - 10 + 'a';
+			result[len - idx - 1] = cp_num % 16 - 10 + 'A';
 		cp_num /= 16;
 		idx++;
 	}
