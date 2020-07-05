a86f25d072fff7f059d9ef180543ec304affc777
feldblume5263 <53016167+feldblume5263@users.noreply.github.com>
2020-07-05 21:02:45 +0900

    update codes

--- 3_ft_printf/test_main.c
+++ 3_ft_printf/test_main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-//#include "ft_printf.h"
+#include "ft_printf.h"
 
 int			main(void)
 {
 	printf("\n");
-	//printf("my result = %d\n", ft_printf("ft_printf = |%10.*d|\n", -7, -234));
-	printf("my result = %d\n", printf("   printf = |%10.*d|\n", 7, -234));
+	printf("my result = %d\n", ft_printf("ft_printf = |%.9d|\n", -234));
+	printf("ra result = %d\n", printf("   printf = |%.9d|\n", -234));
 	printf("\n");
 
 }
