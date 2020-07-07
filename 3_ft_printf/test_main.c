52041fc11a3489ef0c05278a52277a7a50610dee
junhpark <git@vogsphere.42seoul.kr:vogsphere/intra-uuid-5d21ae11-278a-4670-bce7-8fb9e642cdcc-3045007>
2020-07-07 20:57:54 +0900

    update test

--- 3_ft_printf/test_main.c
+++ 3_ft_printf/test_main.c
@@ -6,7 +6,7 @@ int			main(void)
 	char		*ptr;
 
 	printf("\n");
-	printf("my result = %d\n", ft_printf("ft_printf = |%-4.x|\n", 10));
-	printf("ra result = %d\n",    printf("   printf = |%-4.x|\n", 10));
+	printf("my result = %d\n", ft_printf("ft_printf = |%.5s|\n", "abc"));
+	printf("ra result = %d\n",    printf("   printf = |%.5s|\n", "abc"));
 	printf("\n");
 }
