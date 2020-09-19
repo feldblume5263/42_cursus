b5811950f860d973a424f6f64bb2ce4a94d506b9
feldblume5263 <53016167+feldblume5263@users.noreply.github.com>
2020-09-19 15:25:32 +0900

    update code

---
+++ 6_cub3D/includes/cub.h
@@ -0,0 +1,42 @@
+#ifndef CUB_H
+# define CUB_H
+
+# include <stdio.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <math.h>
+# include "mlx.h"
+# include "libft.h"
+
+# define KEY_Q					12
+# define KEY_W					13
+# define KEY_E					14
+# define KEY_A					0
+# define KEY_S					1
+# define KEY_D					2
+# define KEY_LEFT				123
+# define KEY_RIGHT				124
+
+# define X_EVENT_KEY_PRESS		2
+# define X_EVENT_KEY_RELEASE	3
+# define X_EVENT_KEY_EXIT		17
+
+typedef struct	s_img
+{
+	void		*img;
+	int			*data;
+	int			bpp;
+	int			size_l;
+	int			endian;
+	int			img_width;
+	int			img_height;
+}				t_img;
+
+typedef struct	s_game
+{
+	void		*mlx;
+	void		*win;
+	t_img		img;
+}				t_game;
+
+#endif
