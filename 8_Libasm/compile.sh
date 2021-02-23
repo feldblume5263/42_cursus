e5848d51c721205f0fbbab0570b4e29758a586a5
feldblume5263 <feldblume5263@gmail.com>
2021-02-24 01:18:48 +0900

    update makefile

--- 8_Libasm/compile.sh
+++
@@ -1,8 +0,0 @@
-nasm -fmacho64 ft_strlen.s
-nasm -fmacho64 ft_strcpy.s
-nasm -fmacho64 ft_strcmp.s
-nasm -fmacho64 ft_write.s
-nasm -fmacho64 ft_read.s
-nasm -fmacho64 ft_strdup.s
-gcc -O3 -c main.c
-gcc -O3 -o test  *.o
