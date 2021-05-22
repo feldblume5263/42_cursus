2f06b1c5f4f526eb353d7da063e0c2ab097b0eb0
feldblume5263 <feldblume5263@gmail.com>
2021-05-22 22:57:09 +0900

    update code

---
+++ exam02.c
@@ -0,0 +1,56 @@
+char		*gnl_join(char *line, char buff)
+{
+	char	*ret;
+	int		size;
+
+	size = 0;
+	while (line[size])
+		size++;
+	if (!(ret = (char *)malloc(sizeof(char) * (size + 2))))
+		return (0);
+	size = -1;
+	while (line[++size])
+		ret[size] = line[size];
+	ret[size] = buff;
+	ret[size + 1] = '\0';
+	free(line);
+	line = NULL;
+	return (ret);
+}
+
+int			get_next_line(char **line)
+{
+	char	*buff;
+	int		flag;
+
+	buff = (char *)malloc(sizeof(char) * 2);
+	if (!buff || !line || !(*line = (char *)malloc(sizeof(char) * 1)))
+		return (-1);
+	*line[0] = '\0';
+	while ((flag = read(0, buff, 1)) > 0)
+	{
+		if (buff[0] == '\n')
+			break;
+		*line = gnl_join(*line, buff[0]);
+	}
+	free(buff);
+	buff = NULL;
+	return (flag);
+}
+
+int			main(void)
+{
+	char	*line;
+	int		flag;
+
+	while ((flag = get_next_line(&line)) > 0)
+	{
+		printf("%s\n", line);
+		free(line);
+		line = NULL;
+	}
+	printf("%s", line);
+	free(line);
+	line = NULL;
+	return (0);
+}
